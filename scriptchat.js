b6e579fc099aabe2a886e6dbc46450a5bdc43763
Bruno Oliveira <brunogabriel2608@gmail.com>
2022-07-28 19:41:21 -0300

    <FEAT>(Adicionando botao com a funçao de editar mensagem e finalizando o projeto com as formataçoes necessarias)

--- scriptchat.js
+++ scriptchat.js
@@ -16,8 +16,26 @@ function addMensage(){
         excluted.remove()
     })  
     divmsg.appendChild(exc)
-    
-
+     // criaçao de um elemento button e formataçao dele para a funçao editar
+    let edit = document.createElement("button")
+    edit.innerText = "editar"
+    edit.className = "editar"
+    edit.addEventListener("click", function(){
+        let edited = this.parentElement
+        edited.innerHTML = `<textarea name='' id='msg' cols='35' rows='5' placeholder='digite a mensagem...'>${mensagem}</textarea>
+                           <button class='editar' id ='save'>salvar alteraçao</button>`
+        let save = document.querySelector('#save')
+    //    funçao para salvar alteraçao
+        save.addEventListener("click", function(){
+            mss = document.querySelector('#msg').value
+            edited.innerHTML = ''
+            edited.innerText = mss
+            edited.appendChild(edit)
+            edited.appendChild(exc)
+            mensagem = mss
+        })
+    })
+    divmsg.appendChild(edit)
     // adicionando a mensagem no historico
     document.body.querySelector(".mensagens").appendChild(divmsg)
 }
