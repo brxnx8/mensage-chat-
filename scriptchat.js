2330f46ddbfda0736ed91764e5e470ad61ce0b2e
Bruno Oliveira <brunogabriel2608@gmail.com>
2022-07-28 18:43:13 -0300

    <FEAT>(implementado botao funcional  de excluir a mensagem)

--- scriptchat.js
+++ scriptchat.js
@@ -1,11 +1,24 @@
 let vez = 1
 function addMensage(){
+    // pegando a mensagem digitada pelo usuario e removendo da textarea
     let mensagem = document.querySelector("textarea").value
     document.querySelector("textarea").value = ''
+    // criaçao de um elmento div que serve como mensagem e passando a mensagem digitada pelo usuario
     let divmsg = document.createElement("div")
-    divmsg.className = "divmensagem" 
-    divmsg.innerHTML = mensagem + `<button class='excluir' id='msg${vez}'>excluir</button><button class='editar'>editar</button>`
+    divmsg.className = "divmensagem"
+    divmsg.innerText = mensagem
+    // criaçao de um elemento button e formataçao dele para a funçao excluir
+    let exc = document.createElement("button")
+    exc.innerText = "excluir"
+    exc.className = "excluir"
+    exc.addEventListener("click", function(){
+        let excluted = this.parentElement
+        excluted.remove()
+    })  
+    divmsg.appendChild(exc)
+    
+
+    // adicionando a mensagem no historico
     document.body.querySelector(".mensagens").appendChild(divmsg)
-    vez ++
 }
 
