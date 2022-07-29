edd98c54ccbb2a86aa639d85496286f5c813a94a
Bruno Oliveira <brunogso@id.uff.br>
2022-07-28 23:27:25 -0300

    <FEAT>(implementaçao da funçao de cancelar a ediçao e deixando a formataçao do site mais bonita)

--- scriptchat.js
+++ scriptchat.js
@@ -2,7 +2,7 @@ let vez = 1
 function addMensage(){
    let mensagem = document.querySelector(".mensagearea").value
    if (mensagem === ''){
-    window.alert('digite uma mensagem')
+        window.alert('digite uma mensagem')
    }else{
         document.querySelector(".mensagearea").value = ''
         // criaçao de elmentos div que servem como caixa da mensagem e passando a mensagem digitada pelo usuario
@@ -11,32 +11,45 @@ function addMensage(){
         let divmsg = document.createElement("div")
         divmsg.className = "divmensagem"
         divmsg.innerText = mensagem
-        // criaçao de um elemento button e formataçao dele para a funçao excluir
+        // criaçao de um elemento button e implementaçao da funçao excluir
         let exc = document.createElement("button")
-        exc.innerText = "excluir"
+        exc.innerText = "Excluir"
         exc.className = "excluir"
         exc.addEventListener("click", function(){
             let excluted = this.parentElement
             excluted.parentElement.remove()
         })  
         divmsg.appendChild(exc)
-        // criaçao de um elemento button e formataçao dele para a funçao editar
+        // criaçao de um elemento button e implementaçao da funçao editar
         let edit = document.createElement("button")
-        edit.innerText = "editar"
+        edit.innerText = "Editar"
         edit.className = "editar"
         edit.addEventListener("click", function(){
             let edited = this.parentElement
             edited.innerHTML = `<textarea name='' id='msg' cols='35' rows='5' placeholder='digite a mensagem...'>${mensagem}</textarea>
-                            <button class='editar' id ='save'>salvar alteraçao</button>`
-            let save = document.querySelector('#save')
-        //    funçao para salvar alteraçao
-            save.addEventListener("click", function(){
-                mss = document.querySelector('#msg').value
+                            <button class='editar' id ='save'>salvar alteraçao</button> <button class='excluir' id ='cancel'>cancelar</button>`
+            // funçao para cancelar a ediçao
+            let cancel = document.querySelector('#cancel')
+            cancel.addEventListener('click', function(){
                 edited.innerHTML = ''
-                edited.innerText = mss
+                edited.innerText = mensagem
                 edited.appendChild(edit)
                 edited.appendChild(exc)
-                mensagem = mss
+            })
+            // funçao para salvar alteraçao
+            let save = document.querySelector('#save')    
+ 
+            save.addEventListener("click", function(){
+                mss = document.querySelector('#msg').value
+                if(mss === ''){
+                    window.alert('digite uma mensagem')
+                }else{
+                    edited.innerHTML = ''
+                    edited.innerText = mss
+                    edited.appendChild(edit)
+                    edited.appendChild(exc)
+                    mensagem = mss
+                }
             })
         })
         divmsg.appendChild(edit)
